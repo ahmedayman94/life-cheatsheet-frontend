263c98c9eaf3919a1bafde24d21e586237aca15d
ahmedayman94 <ahmedmou@kth.se>
2021-07-25 22:47:38 +0200

    formik form validation for new post

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,4 +1,4 @@
-import { convertFromRaw, convertToRaw, EditorState } from "draft-js";
+import { convertToRaw, Editor, EditorState } from "draft-js";
 import { useFormik } from "formik";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
@@ -6,16 +6,33 @@ import Sidebar from "../components/Sidebar/Sidebar";
 import { createNewPostAsync, getCategories } from "../utils/http-clients";
 import { Category } from "../interfaces/category.model";
 import { Post } from "../interfaces/post.model";
+import * as Yup from "yup";
 
 const CreatePost = () => {
   const [categories, setCategories] = useState<Category[]>([]);
+  useEffect(() => {
+    getCategories()
+      .then((res) => {
+        console.log("getCategories");
+        setCategories(res);
+      })
+      .catch((err) => {});
+  }, []);
+
+  const onEditorChange = (editorState: EditorState) =>
+    formik.setFieldValue("postEditorState", editorState);
 
   const formik = useFormik({
     initialValues: {
-      postTitle: "Add title",
-      postCategory: 0,
+      postTitle: "",
+      postCategory: -1,
       postEditorState: EditorState.createEmpty(),
     },
+    validationSchema: Yup.object().shape({
+      postTitle: Yup.string().min(3).required("Required"),
+      postCategory: Yup.number().min(0),
+      postEditorState: Yup.mixed().required("Required"),
+    }),
     onSubmit: async (values) => {
       let newPost: Post = {
         title: values.postTitle,
@@ -29,7 +46,6 @@ const CreatePost = () => {
       // setPosts((posts) => [...posts, newPost]);
     },
   });
-
   useEffect(() => {
     getCategories()
       .then((res) => {
@@ -38,13 +54,77 @@ const CreatePost = () => {
       .catch((err) => {});
     return () => {};
   }, []);
-
   return (
     <>
       <Navbar />
       <Sidebar categories={categories} />
       <main className="h-100" style={{ marginTop: "56px" }}>
-        <div className="container h-100"></div>
+        <div className="container h-100 pb-5">
+          <form
+            className="h-100 d-flex flex-column pb-5"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="row flex-grow-0">
+              <div className="col-12 mt-5">
+                <h5 className="w-100">
+                  <input
+                    id="postTitle"
+                    name="postTitle"
+                    placeholder="Add title"
+                    className="post-title w-100 p-2"
+                    // style={{ border: "1px solid grey" }}
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.postTitle}
+                  />
+                </h5>
+              </div>
+            </div>
+            <div className="row flex-grow-0">
+              <div className="col-12">
+                <select
+                  className="form-control"
+                  name="postCategory"
+                  id="postCategory"
+                  onChange={formik.handleChange}
+                  value={formik.values.postCategory}
+                >
+                  <option disabled value={-1}>
+                    Select a category
+                  </option>
+                  {categories.map((cat) => (
+                    <option key={cat.id} value={cat.id}>
+                      {cat.title}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="row flex-grow-1">
+              <div className="col-12 p-4">
+                <Editor
+                  placeholder="Add post content"
+                  editorState={formik.values.postEditorState}
+                  onChange={(editorState) => {
+                    onEditorChange(editorState);
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="row flex-grow-0">
+              <div className="col-12">
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  disabled={!formik.dirty || !formik.isValid}
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </main>
     </>
   );

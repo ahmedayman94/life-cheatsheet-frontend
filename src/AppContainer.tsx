3e6618201f88f0c59bd8eea75e9e2fbb20d5edaa
Ahmed Moustafa <ahmedmou@kth.se>
2021-07-28 12:06:54 +0200

    fix height issue and scrollbar for real

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -8,6 +8,8 @@ import Home from "./pages/Home";
 import { getCategories } from "./utils/http-clients";
 import NotFound from "./pages/NotFound";
 
+import './AppContainer.css';
+
 const AppContainer = () => {
   const [categories, setCategories] = useState<Category[]>([]);
   const [activeCategoryId, setActiveCategoryId] = useState<number>();
@@ -26,7 +28,7 @@ const AppContainer = () => {
     <>
       <Navbar />
       <Sidebar activeCategoryId={activeCategoryId} categories={categories} />
-      <main className="h-100" style={{ marginTop: "56px" }}>
+      <main style={{ marginTop: "56px" }}>
         <div className="px-5 py-2 h-100">
           <Switch>
             <Route

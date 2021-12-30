31de6a0878a8ea0b26a9599a23cddbbbde36f936
ahmedayman94 <ahmedmou@kth.se>
2021-12-30 19:01:55 +0200

    login/logout and auth stuff

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import { UserInfo } from "../interfaces/user.model";
+import httpService from "./http.service";
 
 class AuthService {
-
-    async login(idToken: string) {
+    login(idToken: string): Promise<UserInfo> {
         const config = {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
@@ -10,14 +10,23 @@ class AuthService {
         };
         const params = new URLSearchParams()
         params.append('idToken', idToken);
-        try {
-            const res = await axios.post("/api/v1/auth/login/google", params, config);
-            debugger;
-        } catch (exception: any) {
-            debugger;
-        }
+
+        return httpService.axios.post("/api/v1/auth/login/google", params, config).then(res => res.data);
+    }
+
+    logout() {
+        return httpService.axios.post("/api/v1/auth/logout").then(res => res.data);
+    }
+
+    getMyInfo(): Promise<UserInfo | null> {
+        return httpService.axios.get('/api/v1/users/me')
+            .then(res => res.data)
+            .catch(err => {
+                if (err.response.status === 401) return null;
+
+                throw err;
+            });
     }
 }
-
 
 export default new AuthService();

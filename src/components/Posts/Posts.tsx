fbad18f2ed6e1537011eb4b6c15bff6f9dc8ea64
Ahmed Moustafa <ahmedmou@kth.se>
2022-01-23 21:36:41 +0100

    smaller post width for smaller screens

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -3,33 +3,36 @@ import { Post } from "../../interfaces/post.model";
 import Card from "../Card/Card";
 
 export interface PostsProps {
-  posts: Post[];
-  activeCategoryId: number | undefined;
+	posts: Post[];
+	activeCategoryId: number | undefined;
 }
 
 const Posts: React.FunctionComponent<PostsProps> = ({
-  posts,
-  activeCategoryId,
+	posts,
+	activeCategoryId,
 }) => {
-  return (
-    <div className="row">
-      {posts.length === 0 ? (
-        <div className="col-12 mx-4">
-          {activeCategoryId != null ? (
-            <h5>No posts for this category...</h5>
-          ) : (
-            <h5>Welcome home</h5>
-          )}
-        </div>
-      ) : (
-        posts.map((post) => (
-          <div className="col-lg-3 col-md-4 col-sm-6 my-4" key={post.id}>
-            <Card key={post.id} post={post} />
-          </div>
-        ))
-      )}
-    </div>
-  )
+	return (
+		<div className="row">
+			{posts.length === 0 ? (
+				<div className="col-12 mx-4">
+					{activeCategoryId != null ? (
+						<h5>No posts for this category...</h5>
+					) : (
+						<h5>Welcome home</h5>
+					)}
+				</div>
+			) : (
+				posts.map((post) => (
+					<div
+						className="col-xl-3 col-lg-4 col-md-6 col-sm-6 my-4"
+						key={post.id}
+					>
+						<Card key={post.id} post={post} />
+					</div>
+				))
+			)}
+		</div>
+	);
 };
 
 export default Posts;
